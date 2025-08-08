6365ade8db4a6d3ff79a07d113b4caced93e8e9d
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-08-08 12:33:14 -0500

    feat: enhance container factory; add services and repositories for franchise configuration, discount, overpriced, and percent commissions

--- shared/factory/container_factory.py
+++ shared/factory/container_factory.py
@@ -6,22 +6,34 @@ from billing.application.command.card_command_service import CardCommandService
 from billing.application.query.card_query_service import CardQueryService
 from billing.infraestructure.repository.card_repository import CardRepository
 from rental.application.commands.commissions_command_service import CommissionCommandService
+from rental.application.commands.franchise_config_command_service import FranchiseConfigCommandService
+from rental.application.commands.franchise_discount_command_service import FranchiseDiscountCommandService
+from rental.application.commands.franchise_overpriced_command_service import FranchiseOverpricedCommandService
 from rental.application.commands.goal_command_service import GoalCommandService
 from rental.application.commands.module_command_service import ModuleCommandService
+from rental.application.commands.percent_commissions_command_service import PercentCommissionCommandService
 from rental.application.commands.plan_command_service import PlanCommandService
 from rental.application.commands.plan_time_command_service import PlanTimeCommandService
 from rental.application.commands.subscription_command_service import SubscriptionCommandService
 from rental.application.commands.user_goal_command_service import UserGoalCommandService
 from rental.application.queries.commissions_query_service import CommissionQueryService
+from rental.application.queries.franchise_config_query_service import FranchiseConfigQueryService
+from rental.application.queries.franchise_discount_query_service import FranchiseDiscountQueryService
+from rental.application.queries.franchise_overpriced_query_service import FranchiseOverpricedQueryService
 from rental.application.queries.goal_query_service import GoalQueryService
 from rental.application.queries.module_query_service import ModuleQueryService
+from rental.application.queries.percent_commissions_query_service import PercentCommissionQueryService
 from rental.application.queries.plan_query_service import PlanQueryService
 from rental.application.queries.plan_time_query_service import PlanTimeQueryService
 from rental.application.queries.subscription_query_service import SubscriptionQueryService
 from rental.application.user_flow_service import UserFlowService
 from rental.infraestructure.Repositories.commissions_repository import CommissionRepository
+from rental.infraestructure.Repositories.franchise_config_repository import FranchiseConfigRepository
+from rental.infraestructure.Repositories.franchise_discount_repository import FranchiseDiscountRepository
+from rental.infraestructure.Repositories.franchise_overpriced_repository import FranchiseOverpricedRepository
 from rental.infraestructure.Repositories.goal_repository import GoalRepository
 from rental.infraestructure.Repositories.module_repository import ModuleRepository
+from rental.infraestructure.Repositories.percent_commission_repository import PercentCommissionRepository
 from rental.infraestructure.Repositories.plan_repository import PlanRepository
 from rental.infraestructure.Repositories.module_plan_repository import PlanModuleRepository
 from rental.infraestructure.Repositories.plan_time_repository import PlanTimeRepository
@@ -60,6 +72,10 @@ def build_services():
     royalties_repo = RoyaltiesRepository()
     user_repo = UserRepository()
     plan_time_repo=PlanTimeRepository()
+    percent_commission_repo = PercentCommissionRepository()
+    franchise_discount_repo = FranchiseDiscountRepository()
+    franchise_overpriced_repo = FranchiseOverpricedRepository()
+    franchise_config_repo = FranchiseConfigRepository()
     # ---------- INSTANTIACIÓN DE SERVICES ----------
     # Application
     application_command_service = ApplicationCommandService(application_repo)
@@ -109,6 +125,23 @@ def build_services():
     # User
     user_command_service = UserCommandService(user_repo)
     user_query_service = UserQueryService(user_repo)
+
+    # --- Percent Commission ---
+    percent_commission_command_service = PercentCommissionCommandService(percent_commission_repo)
+    percent_commission_query_service = PercentCommissionQueryService(percent_commission_repo)
+
+    # --- Franchise Discount ---
+    franchise_discount_command_service = FranchiseDiscountCommandService(franchise_discount_repo)
+    franchise_discount_query_service = FranchiseDiscountQueryService(franchise_discount_repo)
+
+    # --- Franchise Overpriced ---
+    franchise_overpriced_command_service = FranchiseOverpricedCommandService(franchise_overpriced_repo)
+    franchise_overpriced_query_service = FranchiseOverpricedQueryService(franchise_overpriced_repo)
+
+    # --- Franchise Config ---
+    franchise_config_command_service = FranchiseConfigCommandService(franchise_config_repo)
+    franchise_config_query_service = FranchiseConfigQueryService(franchise_config_repo)
+
 
     user_flow_service = UserFlowService(
         user_goal_command_service=user_goal_command_service,
@@ -166,6 +199,25 @@ def build_services():
         "user_query_service": user_query_service,
         "user_flow_service": user_flow_service,
 
+        # --- Percent Commission ---
+        "percent_commission_command_service": percent_commission_command_service,
+        "percent_commission_query_service": percent_commission_query_service,
+
+        # --- Franchise Discount ---
+        "franchise_discount_command_service": franchise_discount_command_service,
+        "franchise_discount_query_service": franchise_discount_query_service,
+
+        # --- Franchise Overpriced ---
+        "franchise_overpriced_command_service": franchise_overpriced_command_service,
+        "franchise_overpriced_query_service": franchise_overpriced_query_service,
+
+        # --- Franchise Config ---
+        "franchise_config_command_service": franchise_config_command_service,
+        "franchise_config_query_service": franchise_config_query_service,
+
+
+
+
         # Repositorios (por si los necesitas directo)
         "application_repo": application_repo,
         "card_repo": card_repo,
@@ -180,4 +232,8 @@ def build_services():
         "inscription_level_repo": inscription_level_repo,
         "royalties_repo": royalties_repo,
         "user_repo": user_repo,
+        "percent_commission_repo": percent_commission_repo,
+        "franchise_discount_repo": franchise_discount_repo,
+        "franchise_overpriced_repo": franchise_overpriced_repo,
+        "franchise_config_repo": franchise_config_repo,
     }
